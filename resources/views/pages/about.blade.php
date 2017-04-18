638b0eb8b6dcdd3aca7b34b2b46f8e2490df8a18
Jonathan Conde <mail@jonathanconde.com>
2017-04-18 17:53:27 +0200

    minor bug fixes and menu width modification

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,13 @@
+@extends ('layouts.master')
+
+
+@section ('content')
+  <section class="hero">
+    <div class="container">
+      <div class="content">
+        <h1>À propos</h1>
+        <p>Clear thinking, straight talking, fast moving. The kind of people you want on your team.</p>
+      </div>
+    </div>
+  </section>
+@endsection
---
+++ resources/views/pages/contact.blade.php
@@ -0,0 +1,138 @@
+@extends ('layouts.master')
+
+@section('content')
+  <section class="hero">
+    <div class="container">
+      <div class="contact">
+
+        <div class="content">
+          <h1>Contact</h1>
+          <p>Ce n'est jamais trop tôt, demandez-moi n'importe quoi.</p>
+        </div>
+
+        <div class="content">
+          @if (Session::has('success'))
+            <div class="alert btn-success" role="alert">
+              <strong>Envoyé:</strong> {{ Session::get('success') }}
+            </div>
+          @endif
+
+          <form class="form-horizontal" role="form" method="POST" action="{{ url('contact') }}">
+            {{ csrf_field() }}
+
+            <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+              <label for="name" class="col-md-4 control-label">Votre nom</label>
+
+              <div class="col-md-6">
+                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
+
+                @if ($errors->has('name'))
+                  <span class="help-block">
+                    <strong>{{ $errors->first('name') }}</strong>
+                  </span>
+                @endif
+              </div>
+            </div>
+
+            <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+              <label for="email" class="col-md-4 control-label">Votre email</label>
+
+              <div class="col-md-6">
+                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+
+                @if ($errors->has('email'))
+                  <span class="help-block">
+                    <strong>{{ $errors->first('email') }}</strong>
+                  </span>
+                @endif
+              </div>
+            </div>
+
+            <div class="form-group{{ $errors->has('message') ? ' has-error' : '' }}">
+              <label for="message" class="col-md-4 control-label">Ce dont vous avez besoin</label>
+
+              <div class="col-md-6">
+                <textarea id="message" type="text" class="form-control" name="message" value="message" required autofocus></textarea>
+
+                @if ($errors->has('message'))
+                  <span class="help-block">
+                    <strong>{{ $errors->first('message') }}</strong>
+                  </span>
+                @endif
+              </div>
+            </div>
+
+            <div class="form-group">
+              <div class="col-md-8 col-md-offset-4">
+                <button type="submit" class="btn btn-primary">
+                  Envoyer
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+
+        <!-- <div class="callout rule">
+             <span class="text">Donnes-m'en plus!</span>
+             </div>
+
+             <div class="callouts">
+             <a class="third callout pop" href="https://twitter.com/JC0nde" target="_blank">
+             <div class="callout-head">
+             <div class="callout-title">Twitter</div>
+             <div class="callout-icon"><svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 40 40"><path fill="#FFF" d="M12.1 16.2l3.7 3.8-7.9 7.9 4.2 4.2 7.9-7.9 3.8 3.7-8 7.9L20 40l7.9-7.9 4.2-4.2L40 20l-4.2-4.2-7.9 8-3.7-3.8 7.9-7.9-4.2-4.2-7.9 7.9-3.8-3.7 8-7.9L20 0l-7.9 7.9-4.2 4.2L0 20l4.2 4.2"></path></svg></div>
+             </div>
+             <div class="callout-body">
+             <p>The official Laravel development environment. Powered by Vagrant, Homestead gets your entire team on the same page with the latest PHP, MySQL, Postgres, Redis, and more.</p>
+             </div>
+             </a>
+             <a class="third callout pop teal" href="https://ch.linkedin.com/in/jonathanconde" target="_blank">
+             <div class="callout-head">
+             <div class="callout-title">LinkedIn</div>
+             <div class="callout-icon"><svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 40 40"><path fill="#FFF" d="M4.1 40H36c2.2 0 4.1-1.8 4.1-4.1V4.1C40 1.8 38.2 0 35.9 0H4.1C1.8 0 0 1.8 0 4.1V36c0 2.2 1.8 4 4.1 4zM32.6 5.6h3.5v3.5h-3.5V5.6zm0 6.3h3.5v3.5h-3.5v-3.5zm0 6.4h3.5v3.5h-3.5v-3.5zm0 6.3h3.5v3.5h-3.5v-3.5zm0 6.3h3.5v3.5h-3.5v-3.5zm-20-18.6c0-.9.9-1.4 1.7-1L27.7 19c.7.4.7 1.5 0 2l-13.3 7.7c-.7.4-1.7-.1-1.7-1V12.3zM3.9 5.6h3.5v3.5H3.9V5.6zm0 6.3h3.5v3.5H3.9v-3.5zm0 6.4h3.5v3.5H3.9v-3.5zm0 6.3h3.5v3.5H3.9v-3.5zm0 6.3h3.5v3.5H3.9v-3.5z"></path></svg></div>
+             </div>
+             <div class="callout-body">
+             <p>Hundreds (yes, hundreds) of Laravel and PHP video tutorials with new videos added every week. Skim the basics or start your journey to Laravel mastery. All for the price of lunch.</p>
+             </div>
+             </a>                <a class="third callout pop" href="https://github.com/JC0nde" target="_blank">
+             <div class="callout-head">
+             <div class="callout-title">GitHub</div>
+             <div class="callout-icon"><svg xmlns="http://www.w3.org/2000/svg" width="38.4" height="40" viewBox="0 0 38.4 40"><path fill="#FFF" d="M4.9 37.8c-3 0-2.7-1.9-2.7-2.7V4.9c0-.9.2-2.7 2.7-2.7h19.7c.9 0 2.7-.3 2.7 2.7v17.3c.7-.3 1.4-.1 2.2-.3V4.4c0-1.7-.1-4.4-4.4-4.4H4.4C.4 0 0 2.7 0 4.4v31.2C0 37.3.1 40 4.4 40H24c-.7-.7-1.2-1.4-1.8-2.2H4.9zm7.2-17.1c-.8-.2-1.5-.4-2.1-.7v2.2c1.1.5 2.5.8 4.3.8v2.2h1.4v-2.3c1.4-.2 2.5-.5 3.2-1.2.8-.7 1.1-1.5 1.1-2.5 0-.8-.3-1.5-.8-2.1-.5-.5-1.4-1.1-2.7-1.6l-.8-.3v-3.8c1.1.1 2.2.3 3.3.8l.8-1.9c-1.3-.5-2.6-.8-4.1-.9V7.6h-1.4v1.8c-1.3.1-2.3.5-3.1 1.2-.8.6-1.2 1.4-1.2 2.4 0 .9.3 1.6.8 2.2.5.6 1.4 1.2 2.5 1.6l.9.3V21c-.6 0-1.3-.1-2.1-.3zm3.5-3.1c.7.2 1.2.5 1.4.8.3.3.4.6.4 1 0 .9-.6 1.4-1.9 1.6.1-.1.1-3.4.1-3.4zm-2.7-3.7c-.3-.3-.4-.6-.4-1s.2-.7.4-1c.3-.3.7-.4 1.3-.5v3.3c-.6-.2-1.1-.5-1.3-.8zM5.5 33.4h15c-.2-.7-.2-1.4-.2-2.2H5.5v2.2zm24.6-9.8c-4.5 0-8.2 3.7-8.2 8.2s3.7 8.2 8.2 8.2 8.2-3.7 8.2-8.2-3.6-8.2-8.2-8.2zm0 14.8c-3.6 0-6.6-3-6.6-6.6s3-6.6 6.6-6.6c2.1 0 4.1 1 5.3 2.6l-5.3 4.8-2.1-2.1c-.2-.2-.5-.2-.7 0l-.8.7c-.2.2-.2.4 0 .6l3.3 3c0 .2.1.2.2.2s.3-.1.3-.1l5.9-5.4c.3.7.4 1.5.4 2.2.1 3.7-2.8 6.7-6.5 6.7z"></path></svg></div>
+             </div>
+             <div class="callout-body">
+             <p>Make subscription billing painless with built-in Stripe and Braintree integrations. Coupons, swapping subscriptions, cancellations, and even PDF invoices are ready out of the box.</p>
+             </div>
+             </a>
+             </div> -->
+
+      </div>
+    </div>
+  </section>
+  <div id="map">
+    <section class="panel">
+      <p class="intro">
+        E-mail:
+        <a class="first" href="mailto:mail@jonathanconde.com">
+          mail@jonathanconde.com
+        </a>
+        Téléphone:
+        <a href="tel:+41765926194">
+          +41 (0)76 592 61 94
+        </a>
+        <br>
+        <br>
+      </p>
+      <p class="intro">
+        <i class="marker"></i>
+        Avenue du Vieux-Moulin 14<br>1018 Lausanne, Suisse
+      </p>
+      <p class="intro">
+        <a class="btn" href="https://maps.google.com/maps?cid=10652211389274937092" title="Trying to find us? Click here to view a map of our location.">
+          Voir sur Google Maps
+        </a>
+      </p>
+    </section>
+    <div id="location-map"></div>
+  </div>
+
+@endsection
